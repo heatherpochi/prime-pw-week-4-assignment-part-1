38569837998190140b9858c2dd52633e0bf2eeba
Heather Kim <heatherkim0405@gmail.com>
2022-01-04 21:52:22 -0600

    cannot solve Q7

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -70,9 +70,22 @@ console.log(getLast(veggies));
 // 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find
-function find( value, array ){
+let numbers = [ 0.1, 3, 59, 7899, 5, 67]
+function find(value, array){
+  for (let i = 0; i < array.length; i++){
+    if(array[i] === value){
+      return true;
+    } else{
+      return false;
+    }
+  }
+};
 
-}
+console.log(find( 3, numbers));
+
+
+
+
 
 // ----------------------
 // Stretch Goals
